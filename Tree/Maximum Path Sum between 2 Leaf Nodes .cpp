5ea85b604ebd986489521acb65b98e33218ea607
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2021-10-05 12:48:13 +0530

    Update Maximum Path Sum between 2 Leaf Nodes .cpp

--- Tree/Maximum Path Sum between 2 Leaf Nodes .cpp
+++ Tree/Maximum Path Sum between 2 Leaf Nodes .cpp
@@ -1,5 +1,15 @@
-class Solution {
-public:
+Input:      
+           3                               
+         /    \                          
+       4       5                     
+      /  \      
+    -10   4                          
+Output: 16
+Explanation:
+Maximum Sum lies between leaf node 4 and 5.
+4 + 4 + 3 + 5 = 16.class Solution {
+
+    public:
     int sum=INT_MIN;
     int maxiPath(Node *root)
     {

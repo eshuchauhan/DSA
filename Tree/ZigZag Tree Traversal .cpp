69d0d6be906395898fad723c1e7da11e8151504d
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2021-10-15 17:18:38 +0530

    Update ZigZag Tree Traversal .cpp

--- Tree/ZigZag Tree Traversal .cpp
+++ Tree/ZigZag Tree Traversal .cpp
@@ -1,53 +1,40 @@
-class Solution{
-    public:
-    //Function to store the zig zag order traversal of tree in a list.
-    vector <int> zigZagTraversal(Node* root)
-    {
-    	// Code here
-    	vector<int>ans;
-    	if(root==NULL)
-    	{
-    	    return ans;
-    	}
-    	queue<Node*>q;
-    	q.push(root);
-    	
-    	int leftright=1;
-    	
-    	while(q.size()!=0)
-    	{
-    	    vector<int>t;
-    	    int n=q.size();
-    	    
-    	    for(int i=0;i<n;i++)
-    	    {
-    	        Node *x=q.front();
-    	        q.pop();
-    	    
-    	        t.push_back(x->data);
-    	   
-    	        if(x->left!=NULL)
-    	        {
-    	            q.push(x->left);
-    	        }
-    	    
-    	        if(x->right!=NULL)
-    	        {
-    	            q.push(x->right);
-    	        }
-    	    }
-    	    
-    	    if(leftright==1)
-    	    {
-    	        ans.insert(ans.end(),t.begin(),t.end());
-    	    }
-    	    else
-    	    {
-    	        reverse(t.begin(),t.end());
-    	        ans.insert(ans.end(),t.begin(),t.end());
-    	    }
-    	    leftright=1-leftright;
-    	}
-    	return ans;
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        
+        int flag=1;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(q.size()!=0)
+        {
+            int n=q.size();
+            vector<int>v;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* x=q.front();
+                q.pop();
+                
+                v.push_back(x->val);
+                
+                if(x->left!=NULL) q.push(x->left);
+                if(x->right!=NULL) q.push(x->right);
+            }
+            if(flag==1)
+            {
+                ans.push_back(v);
+            }
+            else
+            {
+                reverse(v.begin(),v.end());
+                ans.push_back(v);
+            }
+            flag=1-flag;
+        }
+        return ans;
     }
 };

2f2332ba59c5b7720c4cf85403ffa0a9215e13ef
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2021-10-17 18:09:39 +0530

    Update Leaf at same level .cpp

--- Tree/Leaf at same level .cpp
+++ Tree/Leaf at same level .cpp
@@ -24,3 +24,43 @@ class Solution{
         return ans;
     }
 };
+
+
+
+
+
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    bool check(Node *root)
+    {
+        //Your code here
+        queue<Node*>q;
+        if(root==NULL)
+        {
+            return 0;
+        }
+        q.push(root);
+        while(q.size()!=0)
+        {
+            int count=0;
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                Node *x=q.front();
+                q.pop();
+                if(x->left!=NULL) q.push(x->left);
+                if(x->right!=NULL) q.push(x->right);
+                if(x->left==NULL && x->right==NULL)
+                {
+                    count++;
+                }
+            }
+            if(q.size()>0 && count>0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};

ff0ac7bd84f9384f3e3b465ae819ca2ea026a218
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2021-10-30 13:58:57 +0530

    Create  Activity Selection .cpp

---
+++ Greedy.cpp/Activity Selection .cpp
@@ -0,0 +1,23 @@
+ int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({end[i],start[i]});
+        }
+        sort(v.begin(),v.end());
+        int i=0;
+        int j=1;
+        int ans=1;
+        while(j<n)
+        {
+            if(v[j].second>v[i].first)
+            {
+                ans++;
+                i=j;
+            }
+            j++;
+        }
+        return ans;
+    }

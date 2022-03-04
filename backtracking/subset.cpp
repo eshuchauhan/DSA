c8662438855784dbd9630951611cc3bed7a4cdd7
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2022-03-04 19:19:00 +0530

    Create subset.cpp

---
+++ backtracking/subset.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(vector<int>&a,vector<int>v,vector<vector<int>>&ans,int index)
+    {
+        int n=a.size();
+        
+        if(index==n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        
+        v.push_back(a[index]);
+        helper(a,v,ans,index+1);
+        v.pop_back();
+        helper(a,v,ans,index+1);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& a) {
+        
+        vector<vector<int>>ans;
+        vector<int>v;
+        helper(a,v,ans,0);
+        return ans;
+    }
+};

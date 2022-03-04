720ab0e754f134e58e53b30f1ae813eaca44e4e0
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2022-03-04 19:20:13 +0530

    Update subset.cpp

--- backtracking/subset.cpp
+++ backtracking/subset.cpp
@@ -24,3 +24,31 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    void helper(vector<int>&a,vector<int>v,vector<vector<int>>&ans,int index)
+    {
+        int n=a.size();
+        
+        if(index==n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        helper(a,v,ans,index+1);
+        v.push_back(a[index]);
+        helper(a,v,ans,index+1);
+        v.pop_back();
+        
+    }
+    vector<vector<int>> subsets(vector<int>& a) {
+        
+        vector<vector<int>>ans;
+        vector<int>v;
+        helper(a,v,ans,0);
+        return ans;
+    }
+};

b67459b3dad9600d4b138bfaaa968cdf3ba92aa0
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2022-03-13 21:31:15 +0530

    Create Bipartite Graph DFS.cpp

---
+++ Graph/Bipartite Graph DFS.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+vector<int>adj[100001];
+int vis[100001]={0};
+int col[100001];
+bool dfs(int node,int c)
+{
+    vis[node]=1;
+    col[node]=c;
+    for(auto child:adj[node])
+    {
+        if(vis[child]==0) 
+        {
+            if(dfs(child,c^1)==false)
+            {
+                return false;
+            }
+        }
+        else if(col[child]==col[node])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ll n,m,i,j,x,y;
+    
+    cin>>n>>m;
+    
+    for(i=0;i<m;i++)
+    {
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(vis[i]==0)
+        {
+            bool flag=dfs(i,0);
+            if(flag==false)
+            {
+                cout<<"false"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"true"<<endl;
+}

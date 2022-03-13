3bbff63ad682a0e19bfc7fa23bfe73aeefc602d0
eshuchauhan <73069055+eshuchauhan@users.noreply.github.com>
2022-03-13 18:05:33 +0530

    Create 1. BFS.cpp

---
+++ Graph/1. BFS.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+vector<int>adj[100001];
+int vis[100001]={0};
+
+int main()
+{
+    ll n,m,x,y,i,j;
+    ll no_of_nodes,edges;
+    cin>>no_of_nodes>>edges;
+    
+    for(i=0;i<edges;i++)
+    {
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    
+    vector<ll>v;
+    
+    for(i=0;i<no_of_nodes;i++)
+    {
+        if(vis[i]==0)
+        {
+            queue<ll>q;
+            q.push(i);
+            vis[i]=1;
+            
+            while(q.size()>0)
+            {
+                ll node=q.front();
+                q.pop();
+                
+                v.push_back(node);
+                
+                for(auto it:adj[node])
+                {
+                    if(vis[it]==0)
+                    {
+                        q.push(it);
+                        vis[it]=1;
+                    }
+                }
+            }
+        }
+    }
+    for(auto it:v)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
